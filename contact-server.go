0eccfde3730f95826cb107e04997010508383c86
Fraser McLean <dev@frasermclean.com>
2021-06-25 11:48:36 +1000

    Added Contact struct

--- contact-server.go
+++ contact-server.go
@@ -6,6 +6,26 @@ import (
 	"net/http"
 )
 
+// Data structure to store contact data
+type Contact struct {
+	Id           int
+	FullName     string
+	Email        string
+	PhoneNumbers []string
+}
+
+func init() {
+	c1 := Contact{
+		Id:       1,
+		FullName: "Bob Smith",
+		Email:    "bob@acme.com",
+	}
+	c1.PhoneNumbers = append(c1.PhoneNumbers, "123")
+
+	fmt.Printf("%+v\n", c1)
+}
+
+// Entry point for the program
 func main() {
 	// add handler function
 	http.HandleFunc("/", handler)
@@ -16,8 +36,13 @@ func main() {
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
-// callback function which is called upon receiving a HTTP request
+// Callback function which is called upon receiving a HTTP request
 func handler(w http.ResponseWriter, r *http.Request) {
 	message := "Welcome to Contact Server: %s"
 	fmt.Fprintf(w, message, r.Method)
+}
+
+// Save the specfied contact to disk
+func (c *Contact) save() error {
+	return nil
 }
